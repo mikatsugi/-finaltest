5d604828af74cb61be9ebdcb3abc33d3d9c8bce8
劉紘維 <s1060390@gm.pu.edu.tw>
2021-06-24 19:36:23 +0800

    week

--- final/final/Controllers/ExamController.cs
+++ final/final/Controllers/ExamController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using final.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,6 +20,34 @@ namespace final.Controllers
             ViewBag.name = "劉紘維";
             ViewBag.number = "410603901";
             return View();
+        }
+
+        public ActionResult Week()
+        {
+            return View(new WeekData());
+        }
+        [HttpPost]
+        public ActionResult Week(WeekData week)
+        {
+            if (week.Number == 1) {
+                week.NumberMessage = "星期一";
+            } else if (week.Number == 2) {
+                week.NumberMessage = "星期二";
+            } else if (week.Number == 3) {
+                week.NumberMessage = "星期三";
+            } else if (week.Number == 4) {
+                week.NumberMessage = "星期四";
+            } else if (week.Number == 5) {
+                week.NumberMessage = "星期五";
+            } else if (week.Number == 6) {
+                week.NumberMessage = "星期六";
+            } else if (week.Number == 7) {
+                week.NumberMessage = "星期日";
+            } else {
+                week.NumberMessage = "錯誤輸入";
+            }
+
+            return View(week);
         }
     }
 }
---
+++ final/final/ViewModels/WeekData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace final.ViewModels
+{
+    public class WeekData
+    {
+        public int Number {
+            get;
+            set;
+        }
+
+        public string NumberMessage{
+            get;
+            set;
+        }
+    }
+}
